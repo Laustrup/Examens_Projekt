8a638b7653e8524346237fe4e59797097ce8262f
Laust <laust.bonnesen@mail.com>
2021-04-30 16:14:55 +0200

    Initial commit

---
+++ src/main/java/patrick_laust_ayo/examproject/ExamprojectApplication.java
@@ -0,0 +1,13 @@
+package patrick_laust_ayo.examproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamprojectApplication.class, args);
+    }
+
+}
---
+++ src/main/java/patrick_laust_ayo/examproject/ServletInitializer.java
@@ -0,0 +1,13 @@
+package patrick_laust_ayo.examproject;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ExamprojectApplication.class);
+    }
+
+}
