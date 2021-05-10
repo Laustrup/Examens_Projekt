91a043c587acc4d28f81645124196acf00d5b1d4
Laust <laust.bonnesen@mail.com>
2021-05-10 11:53:59 +0200

    Working on participant repo

--- src/main/java/patrick_laust_ayo/examproject/repositories/ParticipantRepository.java
+++ src/main/java/patrick_laust_ayo/examproject/repositories/ParticipantRepository.java
@@ -1,13 +1,41 @@
 package patrick_laust_ayo.examproject.repositories;
 
-import patrick_laust_ayo.examproject.models.Participant;
+import patrick_laust_ayo.examproject.models.*;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ParticipantRepository extends Repository {
 
     private Participant participant;
+
+    // puts in database with and without return, for the reason of an option for faster opportunity and testing as well
+    public void putProjectInDatabase(Participant participantToInsert, int projectId, int departmentNo) {
+        Connection connection = databaseConnection.getConnection();
+        executeSQLStatement(connection,"INSERT into participant \n" +
+                "VALUES (default, \" "+ participantToInsert.getName() + "\", \"" +participantToInsert.getPosition() +
+                "\", " + projectId + ", " + departmentNo + ";");
+    }
+
+    public Project putProjectInDatabaseWithReturn(Project projectToInsert, int projectId, int departmentNo) {
+        Connection connection = databaseConnection.getConnection();
+        executeSQLStatement(connection,"INSERT into participant \n" +
+                "VALUES (default, \" "+ participantToInsert.getName() + "\", \"" + participantToInsert.getPosition() +
+                "\", " + projectId + ", " + departmentNo + ";");
+        ResultSet res = executeQuery(connection,"SELECT * FROM project WHERE title = \"" + projectToInsert.getTitle() + "\";");
+
+        try {
+            project = new Project(res.getString("title"), res.getString("project_password"),
+                    new ArrayList<Phase>(), new HashMap<>(), projectManager);
+        }
+        catch (Exception e) {
+            System.out.println("Couldn't create a projectmanager from resultSet...\n" + e.getMessage());
+            project = null;
+        }
+
+        return project;
+    }
 
 }
--- src/main/java/patrick_laust_ayo/examproject/services/UserCreator.java
+++ src/main/java/patrick_laust_ayo/examproject/services/UserCreator.java
@@ -3,6 +3,7 @@ package patrick_laust_ayo.examproject.services;
 import patrick_laust_ayo.examproject.models.Department;
 import patrick_laust_ayo.examproject.models.Participant;
 import patrick_laust_ayo.examproject.models.ProjectManager;
+import patrick_laust_ayo.examproject.repositories.ParticipantRepository;
 
 public class UserCreator {
 
@@ -15,7 +16,10 @@ public class UserCreator {
     }
 
     public Participant createParticipant(int id, String name, String position, Department department) {
-        participant = new Participant(id, "Null", "Null", department);
+        ParticipantRepository repo = new ParticipantRepository();
+
+        participant = new Participant(id, new String(), new String(), null);
+
         return participant;
     }
 }
