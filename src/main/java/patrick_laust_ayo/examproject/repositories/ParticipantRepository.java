3a678b2fb31b997b4a5622619ab4d2dbe604b75d
Ayo <ayokrohn@gmail.com>
2021-05-14 16:07:59 +0200

    find participant method (project constructor issues - how much data do we need to find)

--- src/main/java/patrick_laust_ayo/examproject/repositories/ParticipantRepository.java
+++ src/main/java/patrick_laust_ayo/examproject/repositories/ParticipantRepository.java
@@ -41,27 +41,28 @@ public class ParticipantRepository extends Repository {
         return participant;
     }
 
-    //TODO this method
-    public Participant findParticipant(String name) {
+    //TODO create a new Project constructor if less parameters is needed
+    /*public Participant findParticipant(String name) {
+
         ResultSet res = executeQuery("SELECT * FROM participant" +
-                "INNER JOIN department " +
+                "INNER JOIN department ON participant.department_no = department.department_no" + "INNER JOIN project" +
                 "WHERE participant_name = '" + name + "';");
+
 
         try {
             res.next();
 
             Department department = new Department(res.getInt("department_no"),
                     res.getString("location"), res.getString("department_name"));
-            participant = new Participant(res.getInt("participant_id"),res.getString("participant_name"),
-                    res.getString("participant_password"),res.getString("participant_position"),
-                    res.getInt("project_id"),res.getInt("department_no"));
-
-        }
-        catch (Exception e) {
+            participant = new Participant(res.getInt("participant_id"), res.getString("participant_name"),
+                    res.getString("participant_password"), res.getString("participant_position"),
+                    department);
+            Project project = new Project(res.getString("title"), res.getString("project_password"), res.getInt("projectmanager_id"));
+        } catch (Exception e) {
             System.out.println("Couldn't create a participant from resultSet...\n" + e.getMessage());
             participant = null;
         }
 
         return participant;
+    }*/
 }
-
