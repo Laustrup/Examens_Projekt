dc2a4db362e2977bed9401dd4b1362aa5a6c930f
Ayo <ayokrohn@gmail.com>
2021-05-17 11:46:33 +0200

    updateprojectmanagertest2

--- src/test/java/patrick_laust_ayo/examproject/services/UserEditorTest.java
+++ src/test/java/patrick_laust_ayo/examproject/services/UserEditorTest.java
@@ -5,14 +5,16 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import patrick_laust_ayo.examproject.models.Participant;
+import patrick_laust_ayo.examproject.models.ProjectManager;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserEditorTest {
 
+    //UserEditor userEditor = new UserEditor();
 
     @ParameterizedTest
-    @CsvSource(value={"2|qscqsc*21|Gitte|Developer|Lars"}, delimiter = '|')
+    @CsvSource(value={"2|qscqsc*21|Gitte|Developer|Lone"}, delimiter = '|')
     void updateParticipant(int id, String password, String name, String position, String formerName) {
         //Arrange
         UserEditor userEditor = new UserEditor();
@@ -28,7 +30,17 @@ class UserEditorTest {
 
     }
 
-    @Test
-    void updateProjectmanager() {
+    @ParameterizedTest
+    @CsvSource(value = {"Andy bozz|erAsD14'd|Andy Boss"}, delimiter = '|')
+    void updateProjectmanager(String username, String password, String formerUsername) {
+
+        UserEditor userEditor = new UserEditor();
+
+        //act
+        ProjectManager projectManager = userEditor.updateProjectmanager(username,password,formerUsername);
+
+        //assert
+        assertEquals(projectManager.getUsername(), username);
+        assertEquals(projectManager.getPassword(), password);
     }
 }
